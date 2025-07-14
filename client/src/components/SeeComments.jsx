963034be2849caca24376ad7206adf4c0b90d571
aymen-igr <aymenigri@gmail.com>
2025-07-14 22:49:55 +0100

    frontend: adding the see comments feature for the admin's tickets management page

---
+++ client/src/components/SeeComments.jsx
@@ -0,0 +1,64 @@
+export default function SeeComments({setSeeComments}){
+    const comments = [
+        {username:"aymen",created:"2024/05/04",content:"this is just a text for this comment, i'm using it to see it it will brock my widths and hights that i'm scared of, btw this is literlly the first time that i wrote this code by myself and it's fun lol"},
+        {username:"aymen",created:"2024/05/04",content:"this is just a text for this comment, i'm using it to see it it will brock my widths and hights that i'm scared of, btw this is literlly the first time that i wrote this code by myself and it's fun lol"},
+        {username:"aymen",created:"2024/05/04",content:"this is just a text for this comment, i'm using it to see it it will brock my widths and hights that i'm scared of, btw this is literlly the first time that i wrote this code by myself and it's fun lol"},
+        {username:"aymen",created:"2024/05/04",content:"this is just a text for this comment, i'm using it to see it it will brock my widths and hights that i'm scared of, btw this is literlly the first time that i wrote this code by myself and it's fun lol"},
+        {username:"aymen",created:"2024/05/04",content:"this is just a text for this comment, i'm using it to see it it will brock my widths and hights that i'm scared of, btw t"},
+        {username:"aymen",created:"2024/05/04",content:"this is just a text for this comment, i'm using it to see it it will brock my widths and hights that i'm scared of, btw this is literlly the first time that i wrote this code by myself and it's fun lol"},
+        {username:"aymen",created:"2024/05/04",content:"this is just a text for this comment, i'm using it to see it it will brock my widths and hights that i'm scared of, btw thil"},
+        {username:"aymen",created:"2024/05/04",content:"this is just a text for this comment, i'm using it to see it it will brock my widths and hights that i'm scared of, btw this is literlly the first time that i wrote this code by my"},
+        {username:"aymen",created:"2024/05/04",content:"this is just a text for this comment, i'm using it to see it it will brock my widths and hights that i'm scared of, btw this is literlly the first time that i wrote this code by myself and it's fun lol"},
+        {username:"aymen",created:"2024/05/04",content:"this is just a text for this comment, i'm using it to see it it will brock my widths and hights that i'm scared of, btw this is literlly the first time that i wrote this code by myself and it's fun lol"},
+        {username:"aymen",created:"2024/05/04",content:"this is just a text for this comment, i'm using it to see it it will brock my widths and hights that i'm scared of, btw this is literlly the first time that i wrote this code by myself and it's fun lol"},
+        {username:"aymen",created:"2024/05/04",content:"this is just a text for this comment, i'm using it to see it it will brock my widths and hights that i'm scared of, btw this is literlly the first time that i wrote this "},
+        {username:"aymen",created:"2024/05/04",content:"this is just a text for this comment, i'm using it to see it it will brock my widths and hights that i'm scared of, btw this is literlly the first time that i wrote this code by myself and it's fun lol"},
+        {username:"aymen",created:"2024/05/04",content:"this is just a text for this comment, i'm using it to see it it will brock my widths and hights that i'm scared of, btw this is literlly the first time that i wrote this code by myself and it's fun lol"},
+        {username:"aymen",created:"2024/05/04",content:"this is just a text for this comment, i'm using it to see it it will brock my widths and hights that i'm scared of, btw this is literlly the first time that i wrote this code by myself and it's fun lol"},
+        {username:"aymen",created:"2024/05/04",content:"this is just a text for this comment, i'm using it to see it it will brock my widths and hights that i'm scared of, btw this is literlly the first time that i wrote this code by myself and it's fun lol"},
+        {username:"aymen",created:"2024/05/04",content:"this is just a text for this comment, i'm using it to see it it will brock my widths and hights that i'm scared of, btw this is literlly the first time that i wrote this code by myself and it's fun lol"},
+        {username:"aymen",created:"2024/05/04",content:"this is just a text for this comment, i'm using it to see it it will brock my widths and hights that i'm scared of, btw this is literlly the first time that i wrote this code by myself and it's fun lol"},
+        {
+  username: "scroll TestUser",
+  created: "2024/07/15",
+  content: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla.
+
+Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Curabitur sodales ligula in libero. Sed dignissim lacinia nunc. Curabitur tortor. Pellentesque nibh. Aenean quam. In scelerisque sem at dolor. Maecenas mattis. Sed convallis tristique sem. Proin ut ligula vel nunc egestas porttitor. Morbi lectus risus, iaculis vel, suscipit quis, luctus non, massa. Fusce ac turpis quis ligula lacinia aliquet. Mauris ipsum. Nulla metus metus, ullamcorper vel, tincidunt sed, euismod in, nibh.
+
+Quisque volutpat condimentum velit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nam nec ante. Sed lacinia, urna non tincidunt mattis, tortor neque adipiscing diam, a cursus ipsum ante quis turpis. Nulla facilisi. Ut fringilla. Suspendisse potenti. Nunc feugiat mi a tellus consequat imperdiet. Vestibulum sapien. Proin quam. Etiam ultrices. Suspendisse in orci enim.
+
+Vivamus luctus urna sed urna ultricies ac tempor dui sagittis. In condimentum facilisis porta. Sed nec diam eu diam mattis viverra. Nulla fringilla, orci ac euismod semper, magna diam porttitor mauris, quis sollicitudin sapien justo in libero. Vestibulum mollis mauris enim. Morbi euismod magna ac lorem rutrum elementum. Donec viverra auctor lobortis. Pellentesque eu est a nulla placerat dignissim. Morbi a enim in magna semper bibendum. Etiam scelerisque, nunc ac egestas consequat, odio nibh euismod nulla, eget auctor orci nibh vel nisi.
+
+Aliquam erat volutpat. Sed quis velit. Nulla facilisi. Nulla libero. Vivamus pharetra posuere sapien. Nam consectetuer. Sed aliquam, nunc eget euismod ullamcorper, lectus nunc ullamcorper orci, fermentum bibendum enim nibh eget ipsum. Donec porttitor ligula eu dolor. Maecenas vitae nulla consequat libero cursus venenatis. Nam magna enim, accumsan eu, blandit sed, blandit a, eros. Quisque facilisis erat a dui. Nam malesuada ornare dolor. Cras gravida, diam sit amet rhoncus ornare, erat elit consectetuer erat, id egestas pede nibh eget odio. Proin tincidunt, velit vel porta elementum, magna diam molestie sapien, non aliquet massa pede eu diam. Aliquam iaculis. Fusce et metus porttitor nibh pharetra sagittis.`
+}
+    ];
+
+    const commentsList = comments.map((c)=>{
+        return(
+            <div className="flex flex-row justify-start mb-2 bg-gray-200 p-2 rounded-2xl">
+                <div className="flex flex-col items-start mr-1">
+                    <h4 className="text-start font-medium text-black text-2xl">{c.username}:</h4>
+                    <h6 className="text-start font-thin text-gray-500 text-sm">{c.created}</h6>
+                </div>
+                <p className="text-start mb-1 rounded-2xl max-w-[95%] bg-gray-100 p-2">{c.content}</p>
+            </div>
+        )
+    })
+    return(
+        <>
+            <div className="z-25 bg-black opacity-40 w-[200%] h-[200%] fixed bottom-[-10%] left-[-10%]" onClick={()=>{setSeeComments(false)}}></div>
+            <div className="bg-white z-40 text-black fixed top-[9%] left-[24.2%] p-6 rounded-lg font-medium max-w-2xl">
+                <div className="mt-7 mr-7">
+                    <div className=" mt-4 ml-2">
+                        <div className="flex flex-col items-center overflow-y-auto scroll-bar-hide h-100">
+                            {commentsList}
+                        </div>
+                        <button className="bg-gray-500 text-white mr-2 flex mt-2" onClick={()=>{setSeeComments(false)}}>
+                            Back
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
--- client/src/components/SpicificTicket.jsx
+++ client/src/components/SpicificTicket.jsx
@@ -1,28 +1,40 @@
 import AssignTicket from "./AssignTicket";
 import { useState } from "react";
+import SeeComments from "./SeeComments";
 
 export default function SpicificTicket({ticket,setDetails}){
     const [assign,setAssign] = useState(false);
+    const [seeComments,setSeeComments] = useState(false)
     return(
         <>
             <div className="z-25 bg-black opacity-40 w-[200%] h-[200%] fixed bottom-[-10%] left-[-10%]" onClick={()=>{setDetails(false)}}></div>
-            <div className="bg-white z-40 text-black fixed top-[10%] left-[24.5%] p-6 flex justify-center flex-col items-start rounded-lg font-medium max-w-2xl">
+            <div className="bg-white z-40 text-black fixed top-[8%] left-[24%] p-8 flex justify-center flex-col items-start rounded-lg font-medium w-[53%]">
                 <h3 className="mb-3">Id: {ticket.id}</h3>
                 <h3 className="mb-3">Title: {ticket.title}</h3>
                 <div className="flex justify-start">
                    <h3 className="mb-3">Description: </h3> <p>{ticket.description}</p>
                 </div>
-                <h3 className="mb-3">Categorie: {ticket.categorie}</h3>
-                <h3 className="mb-3">Priority: {ticket.priority}</h3>
-                <h3 className="mb-3">Status: {ticket.status}</h3>
                 <h3 className="mb-3">User: {ticket.user}</h3>
                 <h3 className="mb-3">Technician: {ticket.tech}</h3>
+                <h3 className="mb-3">Categorie: {ticket.categorie}</h3>
+                <div className="flex flex-row justify-center items-start w-full">
+                    <label className="mt-4">Priority</label>
+                    <select  placeholder="Role" id="role" className="border w-[95%] border-gray-300 rounded-md p-2 h-10 placeholder-gray-400 text-emerald-950 mr-2 m-3  focus:outline-2 focus:outline-green-500 transition-colors duration-300 ease-in-out font-medium">
+                        <option value="Low">Low</option>
+                        <option value="Madium">Madium</option>
+                        <option value="Hight">Hight</option>
+                        <option value="Critical">Critical</option>
+                    </select>
+                </div>
+                <h3 className="mb-3">Status: {ticket.status}</h3>
                 <h3 className="mb-3">created at: {ticket.created}</h3>
                 <h3 className="mb-3">closed at: {ticket.closed}</h3>
-                <h3 className="mb-3">status: {ticket.status}</h3>
                 <div className="mt-7 mr-7">
                     <button className="bg-green-600 text-white mr-2 " onClick={()=>{setAssign(true)}}>
-                    Assign
+                        Assign
+                    </button>
+                    <button className="bg-yellow-300 text-black mr-2 " onClick={()=>{setSeeComments(true)}}>
+                        Comments
                     </button>
                     <button className="bg-red-600 text-white mr-2">
                         Close ticket
@@ -31,7 +43,8 @@ export default function SpicificTicket({ticket,setDetails}){
                         Close
                     </button>
                 </div>
-                {assign && <AssignTicket setAssign={setAssign} />}        
+                {assign && <AssignTicket setAssign={setAssign} />}  
+                {seeComments && <SeeComments setSeeComments={setSeeComments} />}      
             </div>
         </>
     )
